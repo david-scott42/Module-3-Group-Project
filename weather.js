251af0ea3d12119c86fb3f0fd98149733d1a03d1
Scott Archibald <92810135+sjarchie@users.noreply.github.com>
2022-02-19 16:59:51 -0500

    added weather api

--- weather.js
+++ weather.js
@@ -1,10 +1,59 @@
-import {formElement, newCity, newCountry} from './script.js';
+import {formElement, newCity} from './script.js';
 
 formElement.addEventListener("submit", (e) => { 
     e.preventDefault();
-        //on click, submit "newCily" and "newCountry" to API
+    //     //on click, submit "newCily" and "newCountry" to API
     fetch('http://reqres.in/api/users/1', {
-        newCity, newCountry
+        newCity
     })
     .then(res => console.log(res))
 })
+
+let weather = {
+    apiKey: "edb8e8f237bd04242980477250c51925",
+    
+    fetchWeather: function (city) {
+        fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey)
+            .then((response) => {
+                if (!response.ok) {
+                    alert("City Not Found....Try again!");
+                    // throw new Error("No Information Available");
+                }
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp } = data.main;
+        const { humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp.toFixed() + " °F";
+        document.querySeelctor(".humidity").innerText = "Humidity: " + humidity + " %";
+        document.querySelector(".speed").innerText = "Wind Speed: " + speed + " mph";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/?" + name + "')";
+    },
+    search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+    },  
+    };
+    
+    document.querySelector(".search").addEventListener("click", function () { 
+    weather.search();
+    });
+    
+    document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
+    
+    weather.fetchWeather("Orlando");
