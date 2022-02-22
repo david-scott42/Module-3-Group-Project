dfcb6adfda65a129b9f6f6e34439b12132fc737a
David Scott <David.Scott808@gmail.com>
2022-02-21 18:26:11 -1000

    gps and map working, both set to populate after search

--- geoconvert.js
+++ geoconvert.js
@@ -1,15 +1,16 @@
 // this .js is taking user location input and converting to decimal geo coordinates
 
 
-import {formElement, newCity, newCountry} from './script.js';
+import {formElement, newCity, newState, newCountry} from './script.js';
 
+let gpsElement = document.getElementById("gpsDiv")
 let lat
 let lon
-var camElement = document.getElementById("webcamDiv");
 
 // function geocoding (newCity, newCountry) {
-    formElement.addEventListener("submit", (e) => {
-    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${newCity}%20${newCountry}.json?access_token=pk.eyJ1IjoiZGF2aWQtc2NvdHQ4MDgiLCJhIjoiY2t6cnNqczhjMDN0djMwcGt1M3IyYnl3ciJ9.BqI5l2gwffXQXblvdvN3UA`)
+formElement.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${newCity}%20${newCountry}%20${newState}.json?access_token=pk.eyJ1IjoiZGF2aWQtc2NvdHQ4MDgiLCJhIjoiY2t6cnNqczhjMDN0djMwcGt1M3IyYnl3ciJ9.BqI5l2gwffXQXblvdvN3UA`)
     .then(res => {
         if (res.ok){   //
             // console.log(res.json())
@@ -20,44 +21,14 @@ var camElement = document.getElementById("webcamDiv");
     })
     .then(data => {
         // console.log(data.features[0].center)
-        lat = data.features[0].center[0].toFixed(2)  //Latitude of input location
-        lon = data.features[0].center[1].toFixed(2)  //Longitutde of input location
-        console.log(lat,lon)
+        lat = data.features[0].center[0].toFixed(3)  //Latitude of input location
+        lon = data.features[0].center[1].toFixed(3)  //Longitutde of input location
+        // console.log(lat,lon)
     })
-    .then(data => {
-
-    // on click, submit "lat" and "lon" andfetch API
-        // fetch(`https://api.windy.com/api/webcams/v2/list/nearby=${lat},${lon},250`,{ 
-        // tagged out because the 'nearby' cann isn't working
-
-        fetch(`https://api.windy.com/api/webcams/v2/list/region=US.HI`, {  
-        method: 'GET',
-        withCredentials: true,
-        headers: {
-            "x-windy-key": "DBCA7SRhIWGQeSvEOUONhjJpttmWQzlq",
-            "Content-Type": "application/json"  
-        }
+    .then(gps => {
+        gpsElement.innerHTML = `<h2>Coordinates for ${newCity}:</h2>\nLatitude: ${lat}<br>Longitude: ${lon} <br> <img src="https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/${lat},${lon},10/500x300?access_token=pk.eyJ1IjoiZGF2aWQtc2NvdHQ4MDgiLCJhIjoiY2t6cnNqczhjMDN0djMwcGt1M3IyYnl3ciJ9.BqI5l2gwffXQXblvdvN3UA">` 
     })
-        .then(res => {
-            if (res.ok){
-                // console.log(res.json())
-                return res.json()
-            } else {
-                console.log('API unsuccessful')
-            }
-        })
-        .then(data => {
-            // console.log(data.result.webcams[0])
-            var camName = data.result.webcams[0].title
-            var camID = data.result.webcams[0].id
-            console.log(camID,camName)
-
-//adding an embedded video for the webcam 
-            camElement.innerHTML += `<h3>Local cam for ${camName}</h3>\n
-        <a name="windy-webcam-timelapse-player" data-id="${camID}" href="https://windy.com/webcams/${camID}" target="_blank">${camName}</a>\n<script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>`
-        })    
-    })
-
 })
+
 
 export {lat, lon}
